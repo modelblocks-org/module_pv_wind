1d95fb356a778fd60dfa6b8b69e8878530d6798c
Jann Launer <32454596+jnnr@users.noreply.github.com>
2026-09-19 00:36:55 +0200

    Adapt local test to new output paths

--- tests/local_test.py
+++ tests/local_test.py
@@ -5,7 +5,7 @@ import subprocess
 
 def test_europe_nuts2_onshore(user_path):
     """A larger test case for Europe at NUTS2 spatial resolution."""
-    target = "results/era5_2017/Europe_NUTS2_onshore/raster_europe/capacityfactors_wind_onshore_3MW.nc"
+    target = "results/cutouts/era5_2017/shapes/Europe_NUTS2_onshore/layouts_raster/europe/capacityfactors_wind_onshore_3MW.nc"
     assert subprocess.run(
         f"snakemake --use-conda --cores 4 --configfile=tests/config_local_test.yaml {target}",
         shell=True,
